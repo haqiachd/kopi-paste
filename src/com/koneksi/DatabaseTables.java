ccdd2b8aa890e1a676f1bdb06278284b95ce3b8b
Achmad Baihaqi <haqiachd@gmail.com>
2022-12-08 18:53:31 +0700

    menambahkan class2 koneksi

---
+++ src/com/koneksi/DatabaseTables.java
@@ -0,0 +1,20 @@
+package com.koneksi;
+
+/**
+ * Tabel-tabel yang ada dialam <b>Database</b>
+ *
+ * @author Achmad Baihaqi
+ * @since 2021-06-14
+ */
+public enum DatabaseTables {
+    
+    USERS,
+    PETUGAS,
+    PEMBELI,
+    BARANG,
+    TRANSAKSI_JUAL,
+    TRANSAKSI_BELI,
+    LAPORAN_PENDAPATAN,
+    LAPORAN_PENGELUARAN
+    
+}
