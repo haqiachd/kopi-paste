c73f6a2e667569ceea27fe85a958233a521d76b0
Achmad Baihaqi <haqiachd@gmail.com>
2022-12-15 23:33:34 +0700

    modified table detail

--- src/test/Tes.java
+++ src/test/Tes.java
@@ -5,9 +5,15 @@
  */
 package test;
 
+import com.koneksi.Koneksi;
 import com.manage.Waktu;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,32 +23,142 @@ import java.util.Date;
  */
 public class Tes {
     
+    public String getNama(String idMenu){
+        try{
+            Connection c = (Connection) Koneksi.configDB();
+            Statement s = c.createStatement();
+            ResultSet r = s.executeQuery("SELECT nama_menu FROM menu WHERE id_menu = '"+idMenu+"'");
+            
+            if(r.next()){
+                String nama = r.getString(1);
+                c.close(); s.close(); r.close();
+                return nama;
+            }
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
+    public String getJenis(String idMenu){
+        try{
+            Connection c = (Connection) Koneksi.configDB();
+            Statement s = c.createStatement();
+            ResultSet r = s.executeQuery("SELECT jenis FROM menu WHERE id_menu = '"+idMenu+"'");
+            
+            if(r.next()){
+                String jenis = r.getString(1);
+                c.close(); s.close(); r.close();
+                return jenis;
+            }
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    public String getHarga(String idMenu){
+        try{
+            Connection c = (Connection) Koneksi.configDB();
+            Statement s = c.createStatement();
+            ResultSet r = s.executeQuery("SELECT harga FROM menu WHERE id_menu = '"+idMenu+"'");
+            
+            if(r.next()){
+                String harga = r.getString(1);
+                c.close(); s.close(); r.close();
+                return harga;
+            }
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    public Object[] getIdTrj(){
+        try{
+            Connection c = (Connection) Koneksi.configDB();
+            Statement s = c.createStatement();
+            ResultSet r = s.executeQuery("SELECT * FROM detail_tr_jual");
+            ArrayList<String> list = new ArrayList();
+            
+            while(r.next()){
+                list.add(r.getString("id_tr_jual"));
+            }
+            
+            c.close(); s.close(); r.close();
+            return list.toArray();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    private Object[] getIdMenu(String idTr){
+         try{
+            Connection c = (Connection) Koneksi.configDB();
+            Statement s = c.createStatement();
+            ResultSet r = s.executeQuery("SELECT id_menu FROM detail_tr_jual WHERE id_tr_jual = '"+idTr+"'");
+            ArrayList<String> a = new ArrayList();
+            
+            while(r.next()){
+                String id = r.getString(1);
+                a.add(id);
+            }
+            c.close(); s.close(); r.close();
+            return a.toArray();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    private void updateDtrj(String idTr){
+        try{
+            for(Object idMn : this.getIdMenu(idTr)){
+                String nama = this.getNama(idMn.toString()), 
+                        jenis = this.getJenis(idMn.toString()), 
+                        harga = this.getHarga(idMn.toString());
+                
+                System.out.println(nama);
+                System.out.println(jenis);
+                System.out.println(harga);
+                
+                String sql = String.format("UPDATE detail_tr_jual "
+                        + "SET nama_menu = '%s', jenis_menu = '%s', harga_menu = '%s' "
+                        + "WHERE id_tr_jual = '%s' AND id_menu = '%s' ", nama, jenis, harga, idTr, idMn);
+                Connection c = (Connection) Koneksi.configDB();
+                Statement s = c.createStatement();
+                int result = s.executeUpdate(sql);
+
+                if(result > 0){
+                    System.out.println("update berhasil");
+                }
+                
+                c.close(); s.close();
+            }
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+    }
     
     public static void main(String[] args) throws ParseException {
         
-        Waktu t = new Waktu();
-        Date[] d = t.weekk(24, 11, 2022);
-//        System.out.println("d[0] "+ d[0]);
-//        System.out.println("d[1] "+ d[1]);
         
+        Tes t = new Tes();
+//        System.out.println(t.getIdMenu("TRJ0059"));
         
-        Date dd;
-        Calendar c;
-        int hari = t.getTotalHari(12);
-        for(int i = 0; i <= hari; i++){
-            dd = new Date(i, 11, 2022);
-            c = Calendar.getInstance();
-            c.setTime(dd);
-            int p = c.get(Calendar.DAY_OF_WEEK);
-            if(p == 1){
-                Date[] v = t.weekk(i+1, 11, 2022);
-                System.out.format("\nSENIN  : %d-%d-%d", v[0].getDate(), v[0].getMonth(), v[0].getYear());
-                System.out.format("\nMINGGU : %d-%d-%d\n\n", v[1].getDate(), v[1].getMonth(), v[1].getYear());
-            }
+//        for(Object s : t.getIdMenu("TRJ0059")){
+//            System.out.println(s);
+//        }
+        
+        for(Object o : t.getIdTrj()){
+            System.out.println(o);
+            t.updateDtrj(o.toString());
         }
-        
-        
     }
     
 }
