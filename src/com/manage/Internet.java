743c63d20e1ef3c3e9f7f2040d71cb37bae052f8
Achmad Baihaqi <haqiachd@gmail.com>
2023-04-30 23:40:14 +0700

    config to send email

--- src/com/manage/Internet.java
+++ src/com/manage/Internet.java
@@ -1,16 +1,5 @@
 package com.manage;
 
-import jakarta.mail.Authenticator;
-import jakarta.mail.MessagingException;
-import jakarta.mail.PasswordAuthentication;
-import jakarta.mail.Session;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.Message;
-import jakarta.mail.Multipart;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.MimeBodyPart;
-import jakarta.mail.internet.MimeMultipart;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -20,14 +9,14 @@ import java.net.URISyntaxException;
 import java.util.Properties;
 import javax.swing.JOptionPane;
 
-//import javax.mail.Session;
-//import javax.mail.Authenticator;
-//import javax.mail.PasswordAuthentication;
-//import javax.mail.Transport;
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
+import javax.mail.Session;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Transport;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -36,8 +25,8 @@ import javax.swing.JOptionPane;
 public class Internet {
     
     private final String GMAIL = "kopipaste.app@gmail.com",
-                         RECIPIENT = "hakiahmad756@gmail.com",
-                         PASSWORD = "void main () { print('Halo Dunia') }";
+                         APP_PASS = "dbobgyhspodcwysz",
+                         RECIPIENT = "hakiahmad756@gmail.com";
     
     /**
      * Digunakan untuk mengecek apakah user tersambung ke inernet atau tidak
@@ -59,7 +48,6 @@ public class Internet {
                     s.close();
                 }catch(Exception ex){
                     ex.printStackTrace();
-//                    Apps.showException("Terjadi kesalahan yang tidak diketahui", Apps.class.getName(), ex.toString());
                 }
             }
     }
@@ -81,90 +69,164 @@ public class Internet {
         }
     }
     
-//    public void sendGmail(String subject, String body){
-//            
-//        System.out.println("Mengirim email ke " + RECIPIENT);
-//
-//        // membuat properti object
-//        Properties props = new Properties();
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.host", "smtp.gmail.com");
-//        props.put("mail.smtp.port", "587");
-//
-//        // membuat session
-//        Session session = Session.getInstance(props, new Authenticator(){
-//
-//            @Override 
-//            protected PasswordAuthentication getPasswordAuthentication(){
-//                return new PasswordAuthentication(GMAIL, PASSWORD);
-//            }
-//
-//        });
-//        
-//        // mendebug session
-//        session.setDebug(true);
-//
-//        try{
-//            // membuat email yang akan dikirim
-//            Message message = new MimeMessage(session);
-//            message.setFrom(new InternetAddress(GMAIL)); // mengatur pengirim email
-//            message.setRecipient(Message.RecipientType.TO, new InternetAddress(RECIPIENT)); // mengatur tipe pesan dan penerima email
-//            message.setSubject(subject); // mengatur subject dari email
-//            message.setContent(body, "text/html"); // mengatur isi dari email
-//
-//            Transport.send(message); // mengirimkan email 
-//            System.out.println("Email sukses terkirim ke " + RECIPIENT);
-//
-//        }catch (MessagingException ex) {
-//            com.manage.Message.showException(this, ex);
-//        }
-//
-//    }
-    
-    public void sendGmail(String subject, String body) throws MessagingException{
-        Properties prop = new Properties();
-//        prop.put("mail.smtp.auth", true);
-//        prop.put("mail.smtp.starttls.enable", "true");
-//        prop.put("mail.smtp.host", "smtp.mailtrap.io");
-//        prop.put("mail.smtp.port", "587");
-//        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
+    public boolean sendGmail(String email, String subject, String body){
         
-        Session session = Session.getInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(GMAIL, PASSWORD);
+        if(!this.isConnectInternet()){
+            com.manage.Message.showWarning(this, "Tidak terhubung ke internet!");
+            return false;
+        }
+            
+        System.out.println("Mengirim email ke " + RECIPIENT);
+
+        // membuat properti object
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        // membuat session
+        Session session = Session.getInstance(props, new Authenticator(){
+
+            @Override 
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(GMAIL, APP_PASS);
             }
         });
         
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("from@gmail.com"));
-        message.setRecipients(
-        Message.RecipientType.TO, InternetAddress.parse(RECIPIENT));
-        message.setSubject("Mail Subject");
+        // mendebug session
+        session.setDebug(true);
 
-        String msg = "This is my first email using JavaMailer";
+        try{
+            // membuat email yang akan dikirim
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(GMAIL)); // mengatur pengirim email
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(email)); // mengatur tipe pesan dan penerima email
+            message.setSubject(subject); // mengatur subject dari email
+            message.setContent(body, "text/html"); // mengatur isi dari email
 
-        MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+            Transport.send(message); // mengirimkan email 
+            System.out.println("Email sukses terkirim ke " + RECIPIENT);
+            return true;
 
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(mimeBodyPart);
-
-        message.setContent(multipart);
-
-        Transport.send(message);
-
+        }catch (MessagingException ex) {
+            com.manage.Message.showException(this, ex);
+        }
+        return false;
     }
     
     public static void main(String[] args) throws MessagingException {
         
+        String html = "<!DOCTYPE html>\n" +
+"<html>\n" +
+"<head>\n" +
+"	<title>Verifikasi Email</title>\n" +
+"	<style>\n" +
+"		body {\n" +
+"			font-family: Arial, sans-serif;\n" +
+"			background-color: #f5f5f5;\n" +
+"		}\n" +
+"		h1 {\n" +
+"			color: lightseagreen;\n" +
+"			font-weight: bold;\n" +
+"			margin-top: 40px;\n" +
+"			margin-bottom: 20px;\n" +
+"			text-align: center;\n" +
+"		}\n" +
+"		.info{\n" +
+"			text-align: center;\n" +
+"			font-size: 16px;\n" +
+"			color: slategrey;\n" +
+"			font-weight: bold;\n" +
+"		}\n" +
+"		.info-email{\n" +
+"			text-align: center;\n" +
+"			font-size: 16px;\n" +
+"			color: black;\n" +
+"			font-weight: bold;\n" +
+"		}\n" +
+"		.warning{\n" +
+"			font-size: 16px;\n" +
+"			font-weight: bold;\n" +
+"			color : orangered;\n" +
+"			text-align: center;\n" +
+"			margin-bottom: 20px;\n" +
+"		}\n" +
+"		p {\n" +
+"			color: #777;\n" +
+"			font-size: 16px;\n" +
+"			margin-bottom: 20px;\n" +
+"			text-align: center;\n" +
+"		}\n" +
+"		.code-container {\n" +
+"			background-color: #fff;\n" +
+"			border-radius: 10px;\n" +
+"			box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);\n" +
+"			display: inline-block;\n" +
+"			margin: 0 auto;\n" +
+"			padding: 20px;\n" +
+"			text-align: center;\n" +
+"		}\n" +
+"		.code {\n" +
+"			background-color: #f1f1f1;\n" +
+"			border: none;\n" +
+"			color: dodgerblue; \n" +
+"			font-family: monospace;\n" +
+"			font-size: 32px;\n" +
+"			font-weight: bold;\n" +
+"			padding: 15px;\n" +
+"			text-align: center;\n" +
+"			text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.5);\n" +
+"			width: 200px;\n" +
+"		}\n" +
+"        .footer {\n" +
+"        	background-color: black;\n" +
+"        	color: #fff;\n" +
+"        	padding: 20px;\n" +
+"        	display: flex;\n" +
+"        	justify-content: space-between;\n" +
+"        }\n" +
+"        .footer-left {\n" +
+"        	display: flex;\n" +
+"        	align-items: center;\n" +
+"        }\n" +
+"        .footer-left img {\n" +
+"        	width: 60px;\n" +
+"        	margin-right: 20px;\n" +
+"        }\n" +
+"        .footer-left p {\n" +
+"        	margin: 0;\n" +
+"        	font-size: 14px;\n" +
+"        	color: white;\n" +
+"        }\n" +
+"	</style>\n" +
+"</head>\n" +
+"<body>\n" +
+"	<div class=\"container\">\n" +
+//"		<hr>\n" +
+"		<h1>Kode Verifikasi Email</h1>\n" +
+"		<hr>\n" +
+"		<p class=\"info\">Masukan kode verifikasi dibawah ini pada form Aplikasi</p>\n" +
+"        <center>\n" +
+"            <div class=\"code-container\">\n" +
+"                <div class=\"code\">78392006</div>\n" +
+"            </div>\n" +
+"        </center>\n" +
+"	</div>\n" +
+"	<p class=\"warning\">Kode verifikasi akan kadaluarsa dalam waktu 5 menit.</p>\n" +
+"	\n" +
+"	<p class=\"info-email\">Email ini dikirim secara otomatis mohon jangan balas email ini</p>\n" +
+"\n" +
+"      <div class=\"footer\">\n" +
+"        <div class=\"footer-left\">\n" +
+"          <img src=\"https://polije.ac.id/wp-content/uploads/elementor/thumbs/LOGO-POLITEKNIK-NEGERI-JEMBER-200x200-p501e8qsx93hro564g7wmlj5f1d6bn1idluqt46f2o.png\" alt=\"Logo Kopi Paste\" >\n" +
+"          <p>Copyright © 2022-2023. Cito Team TIF 22 Polije. All rights reserved</p>\n" +
+"        </div>\n" +
+"      </div>\n" +
+"</body>\n" +
+"</html>";
         Internet inet = new Internet();
-        inet.sendGmail("asu", "anjing anda ini");
+        inet.sendGmail("e41222905@student.polije.ac.id", "test 3", html);
         
     }
     
--- src/com/manage/VerifikasiEmail.java
+++ src/com/manage/VerifikasiEmail.java
@@ -1,5 +1,9 @@
 package com.manage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 /**
@@ -10,32 +14,25 @@ public class VerifikasiEmail {
     
     private final Random rand = new Random();
     
+    private final Internet inet = new Internet();
+    
     private final Waktu waktu = new Waktu();
     
-    private static int L_RANDOM = 0, L_KODE = 0, KODE = 0;
+    private static int L_RANDOM = 0, L_KODE = 0;
     
-    public int randomKode(){
-      String kode = "";
-      // cek apakah bisa merandom kode
-      if(this.isCanRandom()){
-          // merandom kode
-          while (kode.length() < 10) {
-              kode += rand.nextInt(10);
-          }
-          
-          L_RANDOM = 30; // reset limit waktu
-          L_KODE = 60; // reset limit aktif kode
-          KODE = Integer.parseInt(kode.substring(0, 8)); // menyimpan reandom kode ke field
-          this.hitungMundurRandom(); // menghitung mundur limit waktu
-          this.hitungMundurKode(); // menghitung mundur limit kode
-          return KODE;
-      }else{
-          // jika masih dalam limit waktu untuk random
-          return this.isActiveKode() ? KODE : -1;
-      }
+    private static String EMAIL;
+    
+    private static String KODE = null;
+    
+    public VerifikasiEmail(String email){
+        EMAIL = email;
     }
     
-    public int getKode(){
+    public VerifikasiEmail(){
+        this(null);
+    }
+    
+    public String getKode(){
         return KODE;
     }
     
@@ -47,11 +44,76 @@ public class VerifikasiEmail {
         return L_KODE;
     }
     
+    public String getEmail(){
+        return EMAIL;
+    }
+    
+    public void setEmail(String email){
+        EMAIL = email;
+    }
+    
+    public String randomKode(){
+      String kode = "";
+      
+      // cek apakah bisa merandom kode
+      if(this.isCanRandom()){
+          // merandom 8 digit kode verifikasi
+          while (kode.length() < 8) {
+              kode += rand.nextInt(10);
+          }
+          
+          // mereset limit random dan limit kode
+          L_RANDOM = 60; 
+          L_KODE = 600;
+          // menyimpan kode verifikasi ke field
+          KODE = kode;
+          // menghitung mundur limit random dan limit kode
+          this.hitungMundurRandom();
+          this.hitungMundurKode(); 
+          // kirim kode ke email
+          boolean send = this.sendEmail();
+          // jika kode gagal dikirim maka kode akan direset
+          if(!send){
+              this.killKode();
+          }
+          
+          return KODE;
+      }else{
+          // jika masih dalam limit random maka akan mereturn kode yang sudah ada
+          return this.isActiveKode() ? KODE : null;
+      }
+    }
+    
+    private boolean sendEmail(){
+        try {
+            String subject = KODE + " adalah kode verifikasi Anda";
+            return this.inet.sendGmail(EMAIL, subject, this.generateHtml());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+    
+    private String generateHtml() throws IOException{
+        File file = new File("src\\resources\\file\\verifikasi-email.html");
+        FileReader fr = new FileReader(file);
+        BufferedReader bf = new BufferedReader(fr);
+        String buffer, html = "";
+        
+        while((buffer = bf.readLine()) != null){
+            if(buffer.contains("00000000")){
+                buffer = buffer.replace("00000000", KODE);
+            }
+            html += buffer;
+        }
+        return html;
+    }
+    
     public void hitungMundurRandom(){
         new Thread(new Runnable(){
             @Override
             public void run(){
-                // hitung mundur limit waktu sampai detik ke 0
+                // hitung mundur limit random sampai detik ke 0
                 while(L_RANDOM > 0){
                     L_RANDOM--;
                     waktu.delay(1000);
@@ -64,10 +126,12 @@ public class VerifikasiEmail {
          new Thread(new Runnable(){
             @Override
             public void run(){
-                // hitung mundur limit waktu sampai detik ke 0
+                // hitung mundur limit kode sampai detik ke 0
                 while(L_KODE >= 0){
+                    // cek apakah limit kode sudah berakhir
                     if(L_KODE == 0){
-                        KODE = 0;
+                        // reset kode menjadi null jika melewati batas limit kode
+                        KODE = null;
                     }
                     L_KODE--;
                     waktu.delay(1000);
@@ -76,28 +140,49 @@ public class VerifikasiEmail {
         }).start();
     }
     
+    /**
+     * Digunakan untuk mengecek apakah user dapat merandom kode atau tidak
+     * Jika user dapat merandom kode maka user tidak dalam waktu limit random,
+     * tetapi jika user tidak dapat merandom maka user dalam limit waktu random
+     * 
+     * @return 
+     */
     public boolean isCanRandom(){
         return L_RANDOM == 0;
     }
     
+    /**
+     * Digunakan untuk mengecek apakah kode yang dirandom sebelumnya masih aktif atau tidak,
+     * jika kode melebihi waktu limit kode maka kode akan tidak aktif lagi
+     * 
+     * @return 
+     */
     public boolean isActiveKode(){
         return L_KODE >= 1;
     }
     
+    /**
+     * Digunakan untuk mereset kode yang sebelumnya dirandom
+     * 
+     */
     public void killKode(){
-        KODE = 0;
+        KODE = null;
         L_RANDOM = 0;
         L_KODE = 0;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        VerifikasiEmail ve = new VerifikasiEmail();
+        VerifikasiEmail ve = new VerifikasiEmail("hakiahmad756@gmail.com");
+        ve.randomKode();
         System.out.println(ve.randomKode());
-        while(true){
-            System.out.println(ve.getKode());
-            ve.waktu.delay(2500);
-        }
+//        ve.generateHtml();
+//        System.out.println(ve.randomKode());
+//        while(true){
+//            ve.randomKode();
+//            System.out.println(ve.getKode());
+//            ve.waktu.delay(1000);
+//        }
     }
     
 }
